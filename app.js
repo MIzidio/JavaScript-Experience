6854baff181dc59065ee6d4589b3dce16ccd2bbb
Matheus Izidio <matheusimeira@gmail.com>
2019-10-22 15:36:25 -0300

    manipulação da DOM

---
+++ app.js
@@ -0,0 +1,14 @@
+// document.getElementById()
+console.log(document.getElementById('task-title'));
+
+// get things from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+// change styling
+document.getElementById('task-title').style.background = '#333';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '10px';
+// document.getElementById('task-title').style.display = 'none';
+
+// change content
